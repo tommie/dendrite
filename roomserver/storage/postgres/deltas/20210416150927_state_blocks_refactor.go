2d71273fdca9536fc7c9e478ca6844cd0afa2ff6
Neil Alexander <neilalexander@users.noreply.github.com>
2021-04-19 13:42:12 +0100

    Don't delete after all, we don't need to as we move the table aside

--- roomserver/storage/postgres/deltas/20210416150927_state_blocks_refactor.go
+++ roomserver/storage/postgres/deltas/20210416150927_state_blocks_refactor.go
@@ -138,13 +138,6 @@ func UpStateBlocksRefactor(tx *sql.Tx) error {
 			}
 			newblocks = append(newblocks, blocknid)
 
-			_, err = tx.Exec(`
-				DELETE FROM roomserver_state_block WHERE state_block_nid=$1
-			`, block)
-			if err != nil {
-				return fmt.Errorf("tx.Exec (delete old block): %w", err)
-			}
-
 			if snapshot != lastsnapshot {
 				var newsnapshot types.StateSnapshotNID
 				err = tx.QueryRow(`
